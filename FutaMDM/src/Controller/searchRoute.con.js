025268d26dd7d9693be2b55b3eaba40d891d1d03
LEVIETLUC <11lucviet@gmail.com>
2024-05-05 03:43:34 +0700

    feat: show result search have a stopover

--- FutaMDM/src/Controller/searchRoute.con.js
+++ FutaMDM/src/Controller/searchRoute.con.js
@@ -12,8 +12,8 @@ const searchByRoute = async (req, res) => {
     console.log(time);
     console.log(roundTrip);
     if(roundTrip == "true"){
-        let routeTmp1 = []
-        let routeTmp2 = []
+        let _routeTmp1 = []
+        let _routeTmp2 = []
         console.log("Route")
         console.log(searchText);
         let routeOutgoing = searchText;
@@ -22,20 +22,19 @@ const searchByRoute = async (req, res) => {
         let tmpStr = await neoDB.getRouteByName(depart, des);
         console.log(tmpStr);
         const _listOneWay = await mongoDB.getProductByNameNoIndex(routeOutgoing);
-        tmpStr.forEach(async (element) => {
+        let promises1 = tmpStr.map(async (element) => {
             let routeTmp = `${depart}-${element}`;
             let tmp1 = await mongoDB.getProductByNameNoIndex(routeTmp);
             let routeTmp2 = `${element}-${des}`;
             let tmp2 = await mongoDB.getProductByNameNoIndex(routeTmp2);
-            //push tmp1 to routeTmp1
             tmp1.forEach(element => {
-                routeTmp1.push(element);
+                _routeTmp1.push(element);
             });
             tmp2.forEach(element => {
-                routeTmp1.push(element);
+                _routeTmp1.push(element);
             });
-
         });
+
         let routeReturn = searchText.split("-").reverse().join("-");
         console.log(routeOutgoing);
         console.log(routeReturn);
@@ -44,25 +43,28 @@ const searchByRoute = async (req, res) => {
         let tmpStr2 = await neoDB.getRouteByName(depart2, des2);
         console.log(tmpStr2);
         const _listReturn = await mongoDB.getProductByNameNoIndex(routeReturn);
-        tmpStr2.forEach(async (element) => {
-            let routeTmp = `${depart}-${element}`;
+        let promises2 = tmpStr2.map(async (element) => {
+            let routeTmp = `${depart2}-${element}`;
             let tmp1 = await mongoDB.getProductByNameNoIndex(routeTmp);
-            let routeTmp2 = `${element}-${des}`;
+            let routeTmp2 = `${element}-${des2}`;
             let tmp2 = await mongoDB.getProductByNameNoIndex(routeTmp2);
-            //push tmp1 to routeTmp1
             tmp1.forEach(element => {
-                routeTmp2.push(element);
+                _routeTmp2.push(element);
             });
             tmp2.forEach(element => {
-                routeTmp2.push(element);
+                _routeTmp2.push(element);
             });
+        });
+
+        Promise.all(promises1).then(() => Promise.all(promises2)).then(() => {
+            // Code here will run after both forEach loops have finished
+            res.render("pages/searchRouteRound", { userid, _listOneWay, _listReturn, searchText, moneyConvert, _routeTmp1, _routeTmp2  });
 
         });
 
         
 
 
-        res.render("pages/searchRouteRound", { userid, _listOneWay, _listReturn, searchText, moneyConvert, routeTmp1, routeTmp2  });
         return;
     }
     else{
@@ -73,29 +75,24 @@ const searchByRoute = async (req, res) => {
         console.log(tmpStr);
     const _listOneWay = await mongoDB.getProductByNameNoIndex(routeOutgoing);
     let _routeList = [];
-    tmpStr.forEach(async (element) => {
+    let promises = tmpStr.map(async (element) => {
         let routeTmp = `${depart}-${element}`;
         console.log(routeTmp)
         let tmp1 = await mongoDB.getProductByNameNoIndex(routeTmp);
         let routeTmp2 = `${element}-${des}`;
         let tmp2 = await mongoDB.getProductByNameNoIndex(routeTmp2);
-        //push tmp1 to routeTmp1
-        
-         // Initialize routeTmp as an array
-
-        //push tmp1 to routeTmp1
         tmp1.forEach(element => {
             _routeList.push(element);
         });
         tmp2.forEach(element => {
             _routeList.push(element);
         });
-
+        console.log(_routeList);
     });
-    console.log(_listOneWay);
-
-    // res.status(200).json({result: [...listName.values()]});
-    res.render("pages/searchRoute", { userid, _listOneWay, searchText, _routeList, moneyConvert });
+    
+    Promise.all(promises).then(() => {
+        res.render("pages/searchRoute", { userid, _listOneWay, searchText, _routeList, moneyConvert });
+    });
     return;
     }
 }
